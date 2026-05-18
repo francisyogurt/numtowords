5252bf78a2d49b6e964f6a832ad66e1d9b9552db
lauren <69864211+francisyogurt@users.noreply.github.com>
2026-05-18 16:53:59 -0500

    Update numtowords.lua

--- numtowords.lua
+++ numtowords.lua
@@ -1,102 +1,78 @@
---!strict
---@tinywingedangel
---05/18/26
-
-const ones: { string } = {
+const onesMap: { string } = { 
 	"one", "two", "three", "four", "five",
-	"six", "seven", "eight", "nine",
-	"ten", "eleven", "twelve", "thirteen",
-	"fourteen", "fifteen", "sixteen", "seventeen",
-	"eighteen", "nineteen"
+	"six", "seven", "eight", "nine", "ten",	
+	"eleven", "twelve", "thirteen", "fourteen",
+	"fifteen", "sixteen", "seventeen", "eighteen",
+	"nineteen"
 }
 
-const tens: { string } = {
-	"twenty", "thirty", "fourty", "fifty",
+const tensMap: { string } = {
+	"twenty", "thirty", "forty", "fifty",
 	"sixty", "seventy", "eighty", "ninety"
 }
 
-const suffixes: { string } = {
-	"thousand", "million", "billion"
-}
+const suffixMap: { string } = { "thousand", "million", "billion" }
 
 local function parse_below_twenty(num: number): string
-	return ones[num]
+	return onesMap[num]
 end
 
 local function parse_below_hundred(num: number): string
-	local tensPlace = (num // 10)
-	local onesPlace = (num % 10)
-
-	local tensDigit = tens[tensPlace - 1]
-	local onesDigit = ones[onesPlace]
-	if onesPlace == 0 then
-		return tensDigit
+	local tens, ones = (num // 10), (num % 10)
+	if ones == 0 then
+		return tensMap[tens - 1]
 	end
-
-	return `{tensDigit}-{onesDigit}`
+	
+	return `{tensMap[tens - 1]}-{parse_below_twenty(ones)}`
 end
 
 local function parse_below_thousand(num: number): string
-	local hundredsPlace = (num // 100)
-	local leftover = (num % 100)
-
-	local hundredsDigit = ones[hundredsPlace]
-	if leftover == 0 then
-		return `{hundredsDigit} hundred`
+	local hundreds, remainder = (num // 100), (num % 100)
+	if remainder == 0 then
+		return `{hundreds} hundred`
 	end
-
-	return `{hundredsDigit} hundred and {parse_below_hundred(leftover)}`
+	
+	return `{parse_below_twenty(hundreds)} hundred and {parse_below_hundred(remainder)}`
 end
 
--- Parse numbers below a thousand (1 - 999)
-local function parse_number(num: number): string
+local function parse_num(num: number): string
 	if num < 20 then
 		return parse_below_twenty(num)
 	end
-	
+		
 	if num < 100 then
 		return parse_below_hundred(num)
 	end
-	
-	if num < 1000 then
-		return parse_below_thousand(num)
-	end
+
+	return parse_below_thousand(num)
 end
 
 local function num_to_words(num: number): string
-	if num == 0 then return "zero" end
+	if num == 0 then return 'zero' end
 	num = math.abs(num)
 	
-	-- Partition the number into base-1000
-	local partitioned: { number } = {}
+	local result: {string} = {}
+	
+	local parts: {string} = {}
 	while num > 0 do
-		table.insert(partitioned, num % 1000)
+		table.insert(parts, (num % 1000))
 		num = (num // 1000)
 	end
 	
-	-- Next, go through each of the partitioned
-	-- numbers and concatenate them together
-	local parts: { string } = {}
-	for idx = #partitioned, 1, -1 do
-		local num = partitioned[idx]
-		local parsed = parse_number(num)
+	local formatString = ""
+	for idx = #parts, 1, -1 do
+		local digit = parts[idx]
+		local parsedNumber = parse_num(digit)
 		
-		local suffix = suffixes[idx - 1]
+		local suffix = suffixMap[idx - 1]
 		if suffix then
-			parsed = `{parsed} {suffix},`
+			parsedNumber = `{parsedNumber} {suffix}`
 		end
 		
-		table.insert(parts, parsed)
+		table.insert(result, parsedNumber)
 	end
 	
-	return table.concat(parts, " ")
+	return table.concat(result, " ")
 end
 
---[[
-	Output:
-		one million, 
-		two hundred and thirty-four thousand, 
-		four hundred and fifty-six
-]]
-
-num_to_words(1_234_456)
+print(num_to_words(123_456))
